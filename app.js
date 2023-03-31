de69446d077cdd995d23d8c485d160de32c35035
FabianUlises <ulisesfab.car@outlook.com>
2023-03-31 16:21:32 -0700

    Added logic to add to list only if input has a value

--- app.js
+++ app.js
@@ -22,3 +22,27 @@ const groceryReducer = (state = initialState.groceries, action) => {
             return state
     }
 };
+// Redux store
+let store = Redux.createStore(groceryReducer);
+// Clear button to clear grocery list
+const clearList = () => {
+    // Clear user input
+    document.querySelector('#newItem').value = '';
+    // Call action to clear list
+    store.dispatch({ type: 'grocery/clear' });
+};
+const newGrocery = (e) => {
+    e.preventDefault();
+    // Get value from user input
+    const item = document.querySelector('#newItem').value;
+    // If input is empty return
+    if(item === '') return
+    // Redux
+    store.dispatch({
+        type: 'grocery/add',
+        text: item
+    });
+    console.log(store.getState());
+};
+grocerySubmit.addEventListener('click', e => newGrocery(e));
+clearBtn.addEventListener('click', clearList);
